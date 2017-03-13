853e198c26ef6e63c9200f279dafb7d62dd0a78c
vanashimko <vana.shimko@gmail.com>
2017-03-13 18:10:34 +0300

    Add classic implementation

--- main.py
+++ main.py
@@ -1,43 +1,15 @@
-from random import randrange
-from collections import defaultdict
 from perceptron import Perceptron
-from drawer import display_result
 
-#  Plain
-LEFT = -50
-RIGHT = 50
-BOTTOM = -50
-TOP = 50
-H_STEP = 1
-V_STEP = 1
-
-CLASSES_COUNT = 4
-VECTOR_SIZE = 2
-TRAIN_DATA = [
-    ([RIGHT, TOP], 0),
-    ([LEFT, TOP], 1),
-    ([LEFT, BOTTOM], 2),
-    ([RIGHT, BOTTOM], 3)
+TRAINING_DATA = [
+    ([0, 0], 0),
+    ([0, 1], 0),
+    ([1, 0], 0),
+    ([1, 1], 1)
 ]
-
-TEST_COUNT = 40
-
-def get_test_vectors(count):
-    result = []*count
-    for i in range(count):
-        x = randrange(LEFT, RIGHT, H_STEP)
-        y = randrange(BOTTOM, TOP, V_STEP)
-        result.append(list([x, y]))
-    return result
+TRAINING_ITERS_COUNT = 5
 
 if __name__ == '__main__':
-    perceptron = Perceptron(CLASSES_COUNT, VECTOR_SIZE)
-    perceptron.train(TRAIN_DATA)
-    result = defaultdict(list)
-    for v in TRAIN_DATA:
-        vector = v[0]
-        result[perceptron.get_class(vector)].append(vector)
-    test_vectors = get_test_vectors(TEST_COUNT)
-    for test_vector in test_vectors:
-        result[perceptron.get_class(test_vector)].append(test_vector)
-    display_result(result)
+    perceptron = Perceptron(TRAINING_DATA, TRAINING_ITERS_COUNT)
+    for x, _ in TRAINING_DATA:
+        print('{} AND {} = {}'.format(x[0], x[1], perceptron.get_class(x)))
+    print('weights: {}'.format(perceptron.weights))
--- perceptron.py
+++ perceptron.py
@@ -5,59 +5,28 @@ class Perceptron:
     BIAS_VALUE = 1
     COEFFICIENT = 1
 
-    def __init__(self, classes_count, vector_size):
-        self.functions_weights = [np.matrix([0] * (vector_size + 1)).transpose()] * classes_count
-        self.classes_count = classes_count
-        self.vector_size = vector_size
-
-    def train(self, sample):
-        step = 0
-        errors = True
-        while errors:
-            step += 1
-            errors = False
-            print('step {}'.format(step))
-            for vector, expected_class in sample:
-                assert len(vector) == self.vector_size
-                vector = np.matrix(self._add_bias(vector))
-                functions_results = self._get_separating_functions_results(vector)
-                if not self._is_true_max(expected_class, functions_results):
-                    print('bad boy')
-                    errors = True
-                    punishment = self.COEFFICIENT * vector.transpose()
-                    new_weights = self.functions_weights[expected_class] + punishment
-                    self.functions_weights[expected_class] = new_weights
-                    for i in range(len(self.functions_weights)):
-                        if i != expected_class:
-                            if functions_results[i] >= functions_results[expected_class]:
-                                self.functions_weights[i] = self.functions_weights[i] - punishment
-                else:
-                    print('good boy')
-                print(self.functions_weights)
-        print('Done!')
-        print('Weights are: {}'.format(self.functions_weights))
+    def __init__(self, training_data, training_iters_count):
+        self.sensors_count = len(training_data[0][0])
+        self.weights = [0] * (self.sensors_count + 1)
+        for _ in range(training_iters_count):
+            for vector, expected_result in training_data:
+                assert len(vector) == self.sensors_count
+                vector = np.array(self._add_bias(vector))
+                actual_result = self._get_decision_result(vector)
+                punishment = (expected_result - actual_result) * vector * self.COEFFICIENT
+                self.weights = self.weights + punishment
 
     def get_class(self, vector):
-        vector = np.matrix(self._add_bias(vector))
-        results = self._get_separating_functions_results(vector)
-        return max(enumerate(results), key=lambda x: x[1])[0]
-
-    def save_weights(self):
-        pass
-
-    def load_weights(self):
-        pass
+        assert len(vector) == self.sensors_count
+        vector = np.array(self._add_bias(vector))
+        return self._get_decision_result(vector)
 
     def _add_bias(self, vector):
         return vector + [self.BIAS_VALUE]
 
-    def _get_separating_functions_results(self, vector):
-        result = []
-        for weight in self.functions_weights:
-            result.append(int(vector * weight))
-        return result
+    def _get_decision_result(self, vector):
+        return self._unit_step(np.dot(vector, self.weights))
 
-    def _is_true_max(self, expected_class, functions_results):
-        expected_max = functions_results[expected_class]
-        others = [x for i, x in enumerate(functions_results) if i != expected_class]
-        return all([x < expected_max for x in others])
+    @staticmethod
+    def _unit_step(x):
+        return 0 if x < 0 else 1
