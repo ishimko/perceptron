1575ed8796704e34321a60cf4d7995f69f5974c0
vanashimko <vana.shimko@gmail.com>
2017-03-13 18:28:58 +0300

    Add example with '5' recognition

--- main.py
+++ main.py
@@ -1,15 +1,33 @@
 from perceptron import Perceptron
 
 TRAINING_DATA = [
-    ([0, 0], 0),
-    ([0, 1], 0),
-    ([1, 0], 0),
-    ([1, 1], 1)
+    (list('111101101101111'), 0),
+    (list('001001001001001'), 0),
+    (list('111001111100111'), 0),
+    (list('111001111001111'), 0),
+    (list('101101111001001'), 0),
+    (list('111100111001111'), 1),
+    (list('111100111101111'), 0),
+    (list('111001001001001'), 0),
+    (list('111101111101111'), 0),
+    (list('111101111001111'), 0)
 ]
-TRAINING_ITERS_COUNT = 5
+
+TEST = [
+    list('111100111000111'),
+    list('111100010001111'),
+    list('111100011001111'),
+    list('110100111001111'),
+    list('110100111001011'),
+    list('111100101001111')
+]
+
+TRAINING_ITERS_COUNT = 3
 
 if __name__ == '__main__':
+    TRAINING_DATA = [(list(map(int, l)), result) for l, result in TRAINING_DATA]
     perceptron = Perceptron(TRAINING_DATA, TRAINING_ITERS_COUNT)
-    for x, _ in TRAINING_DATA:
-        print('{} AND {} = {}'.format(x[0], x[1], perceptron.get_class(x)))
+    TEST = [list(map(int, x)) for x in TEST]
+    for x in TEST:
+        print('{}'.format(perceptron.get_class(x)))
     print('weights: {}'.format(perceptron.weights))
